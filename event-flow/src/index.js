6fb2df8e3243de6d86270e7bf54c642fede3e1ea
Mathias <ma10asw@gmail.com>
2019-10-12 16:03:59 +0200

    verbosify index.js

--- event-flow/src/index.js
+++ event-flow/src/index.js
@@ -6,17 +6,20 @@ import { App } from '@data-ui/event-flow';
 import sampleEvents from '@data-ui/event-flow/build/sampleEvents';
 
 // forms require this import
-import '@data-ui/forms/build/react-select.min.css';
+// import '@data-ui/forms/build/react-select.min.css';
 
-// import readme from '../../node_modules/@data-ui/event-flow/README.md';
 
+// It might be that this is all that is needed to render event-flow :O
 const ResponsiveVis = withScreenSize(({ screenWidth, screenHeight, ...rest }) => (
-    <App width={1000} height={1000} {...rest} />
+    <App width={screenWidth * 0.9} height={screenHeight * 0.9} {...rest} />
 ));
 
 
+// The data set might need to be altered.
+// this snippet seems to delegate different data sets for thew different examples, but we are only
+// trying to render one. This might create errors. (we have no var named example)
+
 export default {
-    // usage: readme,
     // one example per dataset
     examples: Object.keys(sampleEvents).map(name => {
         const dataset = sampleEvents[name];
@@ -35,4 +38,5 @@ const Apple = () => {
     )
 };
 
+// example of rendering the app:
 ReactDOM.render(<App /* to render hello: <Apple */ />, document.querySelector('#root'));
