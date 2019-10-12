eb3d01f962fae20d87eac0fd36458087ef85dad2
Mathias <ma10asw@gmail.com>
2019-10-12 16:40:08 +0200

    Fill event flow example with twentyUsers from sampleEvents

--- event-flow/src/index.js
+++ event-flow/src/index.js
@@ -5,38 +5,20 @@ import ReactDOM from 'react-dom';
 import { App } from '@data-ui/event-flow';
 import sampleEvents from '@data-ui/event-flow/build/sampleEvents';
 
-// forms require this import
-// import '@data-ui/forms/build/react-select.min.css';
 
-
-// It might be that this is all that is needed to render event-flow :O
 const ResponsiveVis = withScreenSize(({ screenWidth, screenHeight, ...rest }) => (
     <App width={screenWidth * 0.9} height={screenHeight * 0.9} {...rest} />
 ));
 
-
-// The data set might need to be altered.
-// this snippet seems to delegate different data sets for thew different examples, but we are only
-// trying to render one. This might create errors. (we have no var named example)
-
-export default {
-    // one example per dataset
-    examples: Object.keys(sampleEvents).map(name => {
-        const dataset = sampleEvents[name];
-
-        return {
-            description: name,
-            example: () => <ResponsiveVis data={dataset.allEvents} />,
-        };
-    }),
-};
-
-// Example of how to render Hello:
-const Apple = () => {
+// Example of event flow:
+const EventFlowExample= () => {
     return(
-        <h1>Hello</h1>
+        <div className={'content'}>
+            <h1>Event-flow example: twenty users</h1>
+            <ResponsiveVis data={sampleEvents['twentyUsers'].allEvents} />
+        </div>
     )
 };
 
 // example of rendering the app:
-ReactDOM.render(<App /* to render hello: <Apple */ />, document.querySelector('#root'));
+ReactDOM.render(<EventFlowExample/>, document.querySelector('#root'));
